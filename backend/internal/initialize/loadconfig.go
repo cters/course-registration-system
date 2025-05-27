3a6cf307a4648b2f20dcfc9de236db16410d4d78
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-05-27 17:17:22 +0700

    update code

--- backend/internal/initialize/loadconfig.go
+++ backend/internal/initialize/loadconfig.go
@@ -20,22 +20,22 @@ func Loadconfig() {
 	viperconfig.SetConfigName(mode)
 	viperconfig.AddConfigPath("./config")
 
-	vipersecrets := viper.New()
-	vipersecrets.SetConfigType("yaml")
-	vipersecrets.SetConfigName(mode)
-	vipersecrets.AddConfigPath("./secrets")
+	// vipersecrets := viper.New()
+	// vipersecrets.SetConfigType("yaml")
+	// vipersecrets.SetConfigName(mode)
+	// vipersecrets.AddConfigPath("./secrets")
 
 	config_err := viperconfig.ReadInConfig()
 	if config_err != nil {
 		errors.Must(global.Logger, config_err, "Error loading configuration")
 	}
 
-	secrets_err := vipersecrets.ReadInConfig()
-	if secrets_err != nil {
-		errors.Must(global.Logger, secrets_err, "Error loading configuration")
-	}
+	// secrets_err := vipersecrets.ReadInConfig()
+	// if secrets_err != nil {
+	// 	errors.Must(global.Logger, secrets_err, "Error loading configuration")
+	// }
 
-	viperconfig.MergeConfigMap(vipersecrets.AllSettings())
+	// viperconfig.MergeConfigMap(vipersecrets.AllSettings())
 
 	//read server configuration
 	fmt.Println("Server Port::", viperconfig.GetInt("server.port"))
