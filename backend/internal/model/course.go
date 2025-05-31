580b6d48301eac4c2b510ca39a78c0ccd338fa33
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-05-31 19:23:37 +0700

    #5 - create course model

---
+++ backend/internal/model/course.go
@@ -0,0 +1,16 @@
+package model
+
+type CourseInput struct {
+	SubjectID         string `json:"subject_id"`
+	CourseTerm        string `json:"course_term"`
+	CourseMaxSlot     int16  `json:"course_max_slot"`
+	CourseCurrentSlot int16  `json:"course_current_slot"`
+}
+
+type Course struct {
+	CourseID          int64  `json:"course_id"`
+	SubjectID         string `json:"subject_id"`
+	CourseTerm        string `json:"course_term"`
+	CourseMaxSlot     int16  `json:"course_max_slot"`
+	CourseCurrentSlot int16  `json:"course_current_slot"`
+}
