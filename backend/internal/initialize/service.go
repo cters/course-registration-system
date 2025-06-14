4965ab4779f5da8331ad03ff1957b0d48f582a3d
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-06-14 19:06:56 +0700

    #7 - Add init service interface

--- backend/internal/initialize/service.go
+++ backend/internal/initialize/service.go
@@ -11,5 +11,12 @@ func InitServiceInterface() {
 	queries := database.New(global.Pdbc)
 	// User Service Interface
 	service.InitUserLogin(impl.UserLoginImpl(queries))
-	service.InitUserAdmin(impl.UserAdminImpl(queries))
+	service.InitUserAdmin(impl.UserAdminImpl(global.Pdbc))
+	service.InitUserInfo(impl.UserInfoImpl(queries))
+	// Course Service Interface 
+	service.InitCourseAdmin(impl.CourseAdminImpl(queries))
+	service.InitCourseInfo(impl.CourseInfoImpl(queries))
+	// Registration Service Interface
+	service.InitRegistrationEnroll(impl.RegistrationEnrollImpl(queries))
+	service.InitRegistrationProcessor(impl.RegistrationProcessorImpl(global.Pdbc))
 }
