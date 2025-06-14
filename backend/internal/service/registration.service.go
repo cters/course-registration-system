eb0e12663cbcb95e5ef23cacf02d3788f360a17f
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-06-14 18:45:33 +0700

    #7 - init registration service

--- backend/internal/service/registration.service.go
+++ backend/internal/service/registration.service.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"context"
+
+	"github.com/QuanCters/backend/internal/model"
+)
+
+type (
+	IRegistrationEnroll interface {
+		Enroll(ctx context.Context, in *model.RegistrationInput) (codeResult int, err error)
+		Withdraw(ctx context.Context, in *model.RegistrationInput) (codeResult int, err error)
+	}
+)
+
+var (
+	localRegistrationEnroll IRegistrationEnroll
+)
+
+func RegistrationEnroll() IRegistrationEnroll {
+	if localRegistrationEnroll == nil {
+		panic("implement localRegistrationEnroll not found for interface IRegistrationEnroll")
+	}
+	return localRegistrationEnroll
+}
+
+func InitRegistrationEnroll(i IRegistrationEnroll) {
+	localRegistrationEnroll = i
+}
