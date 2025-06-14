8301a0597a0bbccbdde40b3cb77c2911de8c659e
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2025-06-14 19:16:56 +0700

    add more http status code

--- backend/pkg/response/httpStatusCode.go
+++ backend/pkg/response/httpStatusCode.go
@@ -1,15 +1,22 @@
 package response
 
 const (
-	ErrCodeSuccess      = 201 // Success
+	ErrCodeSuccess      = 200 // Success
+	ErrCodeCreated      = 201 // Created
+	ErrCodeAccepted     = 202 // Accepted
 	ErrCodeParamInvalid = 203 // Email is invalid
 
-	ErrInvalidToken   = 301 // token is invalid
-	ErrSendEmailOtp   = 303
-	ErrCodeAuthFailed = 405
+	ErrInvalidToken = 301 // token is invalid
+	ErrSendEmailOtp = 303
+
+	ErrCodeBadRequest          = 400
+	ErrCodeAuthFailed          = 405
+	ErrCodeConflict            = 409
+	ErrCodeUnprocessableEntity = 422
 
 	ErrCodeInternal      = 500
 	ErrCodeUserHasExists = 501 // user has already registered
+	ErrCodeUserNotFound  = 504
 
 	ErrCodeSubjectNotFound = 600
 
@@ -19,11 +26,21 @@ const (
 // message
 var msg = map[int]string{
 	ErrCodeSuccess:      "success",
+	ErrCodeCreated:      "Create successful",
+	ErrCodeAccepted:     "Request has been accepted and is being processed",
 	ErrCodeParamInvalid: "Email is invalid",
 	ErrInvalidToken:     "token is invalid",
 	ErrSendEmailOtp:     "Failed to send email OTP",
 
-	ErrCodeUserHasExists: "user has already registered",
+	ErrCodeBadRequest:          "Bad request",
+	ErrCodeAuthFailed:          "Authentication failed",
+	ErrCodeConflict:            "Conflict",
+	ErrCodeUnprocessableEntity: "Unprocessable Entity",
+
 	ErrCodeInternal:      "Internal Server Error",
-	ErrCodeAuthFailed:    "Authentication failed",
+	ErrCodeUserHasExists: "user has already registered",
+	ErrCodeUserNotFound:  "User not found",
+
+	ErrCodeSubjectNotFound: "Subject not found",
+	ErrCodeCourseNotFound:  "Course not found",
 }
